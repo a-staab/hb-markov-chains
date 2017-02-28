338431a15651e8d527c10c1b62c266f9c711056d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-28 15:58:15 -0800

    make chains function added

---
+++ testing.py
@@ -0,0 +1,34 @@
+def open_and_read_file(file_path):
+    """Takes file path as string; returns text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+    contents = open(file_path).read()
+    return contents
+
+
+def make_chains(text_string):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
+
+    chains = {}
+    words = text_string.split()
+    for counter in range(len(words)-2):
+        bi_gram = (words[counter], words[counter + 1])
+        chains[bi_gram] = chains.get(bi_gram, []) + [words[counter + 2]]
+    print chains
+
+all_text = open_and_read_file("green-eggs.txt")
+chains_dict = make_chains(all_text)
+
+print chains_dict
